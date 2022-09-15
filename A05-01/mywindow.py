b0c2f74b3b3ade008751c7e15f0cbfeea3e0e7e4
Thiago Freitas <thiago.gn7@gmail.com>
2022-09-15 10:00:18 -0300

    Pós Aula 07 (programa rodando)

--- A05-01/mywindow.py
+++ A05-01/mywindow.py
@@ -4,20 +4,22 @@ from mycanvas import *
 from mymodel import *
 
 class MyWindow(QMainWindow):
+
     def __init__(self):
         super(MyWindow, self).__init__()
         self.setGeometry(100,100,600,400)
         self.setWindowTitle("MyGLDrawer")
         self.canvas = MyCanvas()
         self.setCentralWidget(self.canvas)
-        # create a model object and pass to canvas
+        #create a model object and pass to canvas
         self.model = MyModel()
         self.canvas.setModel(self.model)
-        # create a Toolbar
+        #create a toolbar
         tb = self.addToolBar("File")
-        fit = QAction(QIcon("icons/fit.jpg"),"fit",self)
+        fit = QAction(QIcon("fit.png"), "fit", self)
         tb.addAction(fit)
         tb.actionTriggered[QAction].connect(self.tbpressed)
-    def tbpressed(self,a):
+    
+    def tbpressed(self, a):
         if a.text() == "fit":
             self.canvas.fitWorldToViewport()
